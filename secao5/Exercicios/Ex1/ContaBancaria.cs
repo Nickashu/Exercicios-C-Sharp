8ddee69743765db099961daa2414208625ca491e
Nícolas Arruda <nicolasdamotaarruda@hotmail.com>
2022-06-06 22:17:12 -0300

    Mais exercícios

---
+++ secao5/Exercicios/Ex1/ContaBancaria.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Ex1 {
+    class ContaBancaria {
+
+        public int Numero { get; private set; }
+        public string NomeTitular { get; set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria() {
+        }
+        public ContaBancaria(int numero, string nomeTitular) {   //Caso não haja depósito inicial
+            Numero = numero;
+            NomeTitular = nomeTitular;
+            Saldo = 0;
+        }
+        public ContaBancaria(int numero, string nomeTitular, double depositoInicial) {
+            Numero = numero;
+            NomeTitular = nomeTitular;
+            Saldo += depositoInicial;
+        }
+
+
+        public void Saque(double valor) {
+            int taxa = 5;
+            Saldo = Saldo - taxa - valor;
+            Console.WriteLine("\nDados Atualizados:\n{0}\n", this.ToString());
+        }
+        public void Deposito(double valor) {
+            Saldo += valor;
+            Console.WriteLine("\nDados Atualizados:\n{0}\n", this.ToString());
+        }
+
+        public override string ToString() {
+            return "Conta " + Numero + ", Titular: " + NomeTitular + ", Saldo: $" + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ secao5/Exercicios/Ex1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex1 {
+    class Program {
+        static void Main(string[] args) {
+
+            int numConta;
+            string nomeTitular;
+
+            Console.Write("Número da conta: ");
+            numConta = int.Parse(Console.ReadLine());
+            Console.Write("Titular da conta: ");
+            nomeTitular = Console.ReadLine();
+            string opcDepositInic;
+            while (true) {
+                Console.Write("Haverá depósito inicial? (S/N): ");
+                opcDepositInic = Console.ReadLine();
+                if (opcDepositInic.ToUpper() != "S" && opcDepositInic.ToUpper() != "N") 
+                    Console.WriteLine("Opção inválida! Tente novamente.");
+                else
+                    break;
+            }
+            ContaBancaria conta;
+            if(opcDepositInic.ToUpper() == "S") {
+                Console.Write("Valor do depósito: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(numConta, nomeTitular, depositoInicial);
+            }
+            else 
+                conta = new ContaBancaria(numConta, nomeTitular);
+
+            Console.WriteLine("\nDados da conta:\n{0}\n", conta.ToString());
+
+            int opcao;
+            do {
+                Console.Write("1) Depósito\n2) Saque\n3) Finalizar\nDigite sua opção: ");
+                opcao = int.Parse(Console.ReadLine());
+                switch (opcao) {
+                    case 1:
+                        Console.Write("Valor do depósito: ");
+                        double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                        conta.Deposito(deposito);
+                        break;
+                    case 2:
+                        Console.Write("Valor do saque: ");
+                        double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                        conta.Saque(saque);
+                        break;
+                    case 3:
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida! Tente novamente.");
+                        break;
+                }
+
+            } while (opcao != 3);
+
+        }
+    }
+}
